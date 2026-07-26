4becc6ea648665537ff099620c91802a2ff607ba
bharathos <bharathosinbarca@gmail.com>
2026-07-26 20:56:18 +0530

    add loading state in auth bloc, add githubProvider scope implement github api remote functions.

--- lib/presentation/bloc/auth/auth_bloc.dart
+++ lib/presentation/bloc/auth/auth_bloc.dart
@@ -1,8 +1,6 @@
 import 'package:dev_hub/domain/entities/user_entity.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../core/errors/failures.dart';
 import '../../../domain/usecases/auth/auth_usecase.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
@@ -11,8 +9,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthUseCase _authUseCase;
   AuthBloc(this._authUseCase) : super(AuthInitial()) {
     on<AuthSignUp>((event, emit) async {
+      emit(AuthLoading());
       final result = await _authUseCase.call(null);
-
       result.fold(
         (failure) => emit(AuthFailure(failure.message)),
         (accessToken) => emit(AuthSuccess(accessToken)),
