a80e5fad4dbee4668f7be4b11f884de613227469
Bharath-OS <bharathosinkerala@gmail.com>
2026-08-28 03:06:44 +0530

    feat(membership): return InviteMemberResult from use case branches
    Update InviteMemberToWorkspaceUseCase to return the sealed
    InviteMemberResult instead of a bool.
    
    - When the invitee is not a member, send the org invitation via
      sendOrgInvitation(params) and wrap it in InvitationSentSuccess
    - When already a member, grant repo access via addRepositoryCollaborator
      and wrap it in MemberAddedSuccess

--- lib/features/membership/domain/usecases/invite_member_to_workspace_usecase.dart
+++ lib/features/membership/domain/usecases/invite_member_to_workspace_usecase.dart
@@ -1,16 +1,19 @@
 import 'package:dev_hub/core/errors/failures.dart';
+import 'package:dev_hub/features/membership/domain/entity/invite_member_result.dart';
 import 'package:dev_hub/features/membership/domain/repository/membership_repository.dart';
 import 'package:dev_hub/features/membership/params/invitation_params.dart';
 import 'package:fpdart/fpdart.dart';
 import '../../../../core/usecases/usecase.dart';
 
 class InviteMemberToWorkspaceUseCase
-    implements UseCase<bool, InvitationParams> {
+    implements UseCase<InviteMemberResult, InvitationParams> {
   final MembershipRepository _repository;
   InviteMemberToWorkspaceUseCase(this._repository);
 
   @override
-  Future<Either<Failure, bool>> call(InvitationParams params) async {
+  Future<Either<Failure, InviteMemberResult>> call(
+    InvitationParams params,
+  ) async {
     final checkMemberResult = await _repository.checkMembership(
       orgName: params.orgName!,
       userName: params.userName!,
@@ -18,27 +21,16 @@ class InviteMemberToWorkspaceUseCase
     return checkMemberResult.fold((failure) => left(failure), (isMember) async {
       if (!isMember) {
         //here have to add the user id or user email.
-        final sendOrgInviteResult = await _repository.sendOrgInvitation(
-          orgName: params.orgName!,
-        );
-        return sendOrgInviteResult.fold(
+        final invitationResult = await _repository.sendOrgInvitation(params);
+        return invitationResult.fold(
           (failure) => left(failure),
-          (didSendOrgInvitation) => right(didSendOrgInvitation),
+          (invitation) => right(InvitationSentSuccess(invitation)),
         );
       }
-      final addRepoCollaboratorResult = await _repository
-          .addRepositoryCollaborator(
-            repoName: params.repoName!,
-            ownerName: params.ownerName!,
-            userName: params.userName!,
-            role: params.role!,
-          );
-      return addRepoCollaboratorResult.fold(
-        (failure) => left(failure),
-        (isSuccess) {
-          return right(isSuccess);
-        },
-      );
+      final accessResult = await _repository.addRepositoryCollaborator(params);
+      return accessResult.fold((failure) => left(failure), (member) {
+        return right(MemberAddedSuccess(member));
+      });
     });
   }
 }
